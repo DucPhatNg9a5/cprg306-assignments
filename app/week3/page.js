f39491b781c2e19ae86e15e9c00b2f9b1f557c29
Duc Phat Nguyen <56929794+DucPhatNg9a5@users.noreply.github.com>
2023-09-22 09:55:40 -0600

    week3 added
    week 3 assignment finished

---
+++ app/week3/page.js
@@ -0,0 +1,10 @@
+import ItemList from "./item-list";
+
+export default function main() {
+  return (
+    <main class="bg-slate-950">
+        <h1 class="text-3xl font-bold m-2">Shopping List</h1>
+        <ItemList />
+    </main>
+  );
+}
